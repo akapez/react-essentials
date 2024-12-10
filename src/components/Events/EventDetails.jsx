abf883abe48b960ff5f301232f23f844d3435994
Avishka <avishkakapuruge@gmail.com>
2024-12-10 17:44:13 +0530

    update section 25

---
+++ src/components/Events/EventDetails.jsx
@@ -0,0 +1,84 @@
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
+
+import Header from '../Header.jsx';
+
+import { useQuery, useMutation } from '@tanstack/react-query';
+import { deleteEvent, fetchEvent, queryClient } from '../../utils/http.js';
+
+export default function EventDetails() {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id })
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['events'],
+        refetchType: 'none'
+      });
+      navigate('/events');
+    }
+  });
+
+  const handleDelete = () => {
+    mutate({ id: params.id });
+  };
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id='event-details-content' className='center'>
+        <p>Fetching event data..</p>
+      </div>
+    );
+  }
+
+  if (isError) {
+    content = (
+      <div id='event-details-content' className='center'>
+        <ErrorBlock title="An error occurred" message={error.info?.message || "Failed to fetch event"} />
+      </div>
+    );
+  }
+
+  if (data) {
+    content = <>
+      <header>
+        <h1>{data.title}</h1>
+        <nav>
+          <button onClick={handleDelete}>Delete</button>
+          <Link to="edit">Edit</Link>
+        </nav>
+      </header>
+      <div id="event-details-content">
+        <img src={`http://localhost:3000/${data.image}`} alt={data.image} />
+        <div id="event-details-info">
+          <div>
+            <p id="event-details-location">{data.location}</p>
+            <time dateTime={`Todo-DateT$Todo-Time`}>{data.date} @ {data.time}</time>
+          </div>
+          <p id="event-details-description">{data.description}</p>
+        </div>
+      </div></>;
+  }
+
+  return (
+    <>
+      <Outlet />
+      <Header>
+        <Link to="/events" className="nav-item">
+          View all Events
+        </Link>
+      </Header>
+      <article id="event-details">
+        {content}
+      </article>
+    </>
+  );
+}
