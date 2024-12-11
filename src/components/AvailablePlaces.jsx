e6611af07188fbbb69f3a170cde4573b17494dd3
Avishka <avishkakapuruge@gmail.com>
2024-12-11 10:59:27 +0530

    update section 16

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,43 @@
+import Places from './Places';
+import Error from './Error';
+import { sortPlacesByDistance } from '../loc';
+import { fetchAvailablePlaces } from '../http';
+
+import { useFetch } from "../hooks/useFetch";
+
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      resolve(sortedPlaces);
+    });
+  });
+}
+
+
+export default function AvailablePlaces({ onSelectPlace }) {
+
+  const { isFetching, error, data: availablePlaces } = useFetch(fetchSortedPlaces, []);
+
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
