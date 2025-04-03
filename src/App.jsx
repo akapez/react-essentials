d31e66f9a5d37feb9f46809a3c07c27539e33363
Avishka <avishkakapuruge@gmail.com>
2025-04-03 21:30:53 +0530

    update from actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-
+import Header from "./components/Header";
+import Signup from "./components/Signup";
 
 function App() {
-
   return (
     <>
-      <h1>Hello World!</h1>
+      <Header />
+      <main>
+        <Signup />
+      </main>
     </>
   );
 }
